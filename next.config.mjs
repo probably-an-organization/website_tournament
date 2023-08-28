045203d20ee816c6d707bcb4aed5d1c6f9c16cdc
Futshi <yan.development@outlook.com>
2023-08-28 11:40:06 +0200

    eh

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-import env from "./src/env.js";
+import env from "./src/env.mjs";
 !process.env.SKIP_ENV_VALIDATION && env;
 
 // import { i18 } from "./next-i18next.config.js";
 
 // You can remove the following 2 lines when integrating our example.
-import { nextUtilsConfig } from "./next-utils.config.js";
+import { nextUtilsConfig } from "./next-utils.config.mjs";
 const { esmExternals = false, tsconfigPath } = nextUtilsConfig();
 
 /** @type {import('next').NextConfig} */
